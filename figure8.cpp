aa3f380f15b917dd7bc9283a262c4ed407f802cc
zxl5344 <zxl5344@gmail.com>
2021-12-01 14:14:58 -0500

    add desire function in F8

--- figure8.cpp
+++ figure8.cpp
@@ -1,38 +1,59 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <sensor_msgs/JointState.h>
-#include </home/zxl5344/IROS2022/src/iiwa_ros/koopman-op/src/SAC.hpp>
-#include </home/zxl5344/IROS2022/src/iiwa_ros/koopman-op/src/rk4_int.hpp>
+#include"src/error_cost.hpp"
+#include"src/SAC.hpp"
+#include"src/rk4_int.hpp"
+#include"src/koopsys.hpp"
+#include"src/iiwabasis1.hpp"
 
 #include <sstream>
 #include <iostream>
 #include <fstream>
 #include<math.h>
 #include<armadillo>
-using namespace std;
+
+//desire traj
+arma::vec xdk(double t){
+	double x = 600;
+	double y = 250*cos((1/5)*t);
+	double z = 100*sin((2/5)*t)+700;
+	double wx = 1;
+	double wy = 0;
+	double wz = 0;
+	double w = 0;
+	return arma::vec{x, y, z, wx, wy, wz, w};
+}
 
 class figure8 {
 	private:
 	ros::Subscriber sub;
 	
 	public:
-	figure8(ros::NodeHandle *nh){
+	figure8(ros::NodeHandle *nh, KoopSys<BasisName> systK){
 		sub = nh->subscribe("/iiwa/joint_states", 1000, &figure8::chatterCallback, this);
 	}
 	
 	void chatterCallback(sensor_msgs::JointState jointState){
-		cout << jointState;
+		std::cout << jointState;
 	}
 		
 };
 
 
 
+
 int main(int argc, char **argv){
 	ros::init(argc, argv,"figure8");
 	ros::NodeHandle nh;
+	BasisName iiwaBasis;
+	KoopSys<BasisName> systK (0.01, &iiwaBasis);
 	
-	figure8 nc = figure8(&nh);
+	arma::mat R = 0.001*arma::eye(7,7);
+ 	arma::mat Qk = 200*arma::eye(14,14);
+    
+	
+	figure8 nc = figure8(&nh, systK);
 	
 	ros::spin();
 	
